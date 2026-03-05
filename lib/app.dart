c5a2dd899901c5221fbbd18478ba77ac3bfc40a7
Osama Selim <osamaselim216@gmail.com>
2026-03-05 23:15:57 +0200

    feat: integrate easy_localization into the app shell

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:craft_chain/core/navigation/app_router.dart';
 import 'package:craft_chain/core/theme/app_theme.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CraftChainApp extends StatelessWidget {
   const CraftChainApp({super.key});
@@ -14,6 +16,14 @@ class CraftChainApp extends StatelessWidget {
       darkTheme: AppTheme.dark(),
       themeMode: ThemeMode.light,
       routerConfig: appRouter,
+      locale: context.locale,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: [
+        ...context.localizationDelegates,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:craft_chain/app.dart';
 import 'package:craft_chain/core/di/injection.dart';
 import 'package:craft_chain/features/auth/viewmodels/auth_cubit/auth_cubit.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   configureDependencies();
   runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (_) => getIt<AuthCubit>()),
-      ],
-      child: const CraftChainApp(),
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      startLocale: const Locale('ar'),
+      child: MultiBlocProvider(
+        providers: [BlocProvider(create: (_) => getIt<AuthCubit>())],
+        child: const CraftChainApp(),
+      ),
     ),
   );
 }
