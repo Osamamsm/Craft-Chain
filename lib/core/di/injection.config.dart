66c3f05f084e569fa880f60d9208a01b7999d389
Osama Selim <osamaselim216@gmail.com>
2026-04-06 23:44:05 +0200

    chore(barter): register barter cubits in dependency injection

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -13,6 +13,8 @@ import 'package:craft_chain/core/logic/image_picker_cubit/image_picker_cubit.dar
     as _i173;
 import 'package:craft_chain/features/auth/viewmodels/auth_cubit/auth_cubit.dart'
     as _i454;
+import 'package:craft_chain/features/barter/viewmodels/barter_request_cubit/barter_request_cubit.dart'
+    as _i321;
 import 'package:craft_chain/features/matching/view_model/match_feed_cubit/match_feed_cubit.dart'
     as _i1036;
 import 'package:craft_chain/features/profile/viewmodels/profile_cubit/profile_cubit.dart'
@@ -31,6 +33,7 @@ extension GetItInjectableX on _i174.GetIt {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
     gh.factory<_i173.ImagePickerCubit>(() => _i173.ImagePickerCubit());
     gh.factory<_i454.AuthCubit>(() => _i454.AuthCubit());
+    gh.factory<_i321.BarterRequestCubit>(() => _i321.BarterRequestCubit());
     gh.factory<_i1036.MatchFeedCubit>(() => _i1036.MatchFeedCubit());
     gh.factory<_i752.ProfileCubit>(() => _i752.ProfileCubit());
     gh.factory<_i97.ProfileSetupCubit>(() => _i97.ProfileSetupCubit());
