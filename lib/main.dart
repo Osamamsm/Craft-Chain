dd08152257cf8176c2c7f366a87aa310bc110736
Osama Selim <osamaselim216@gmail.com>
2026-09-09 00:00:55 +0300

    add supabase to the project and initialize it

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:craft_chain/app.dart';
 import 'package:craft_chain/core/di/injection.dart';
 import 'package:craft_chain/features/auth/viewmodels/auth_cubit/auth_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:material_ui/material_ui.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    publishableKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+  );
   await EasyLocalization.ensureInitialized();
   configureDependencies();
   runApp(
