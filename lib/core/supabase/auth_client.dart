c188c89d31e685b3b6c352fdf447b1952c63824b
Osama Selim <osamaselim216@gmail.com>
2026-09-12 23:58:36 +0300

    implement AuthClient interface with SupabaseAuthClientImpl class

---
+++ lib/core/supabase/auth_client.dart
@@ -0,0 +1,25 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract class AuthClient {
+  Future<AuthResponse> signUp({
+    required String email,
+    required String password,
+    required String name,
+  });
+
+  Future<AuthResponse> signIn({
+    required String email,
+    required String password,
+  });
+
+  Future<void> resetPasswordForEmail({required String email});
+
+  Future<AuthResponse> verifyPasswordResetOtp({
+    required String email,
+    required String otp,
+  });
+
+  Future<UserResponse> updatePassword({required String password});
+
+  Future<bool> signInWithOAuth(OAuthProvider provider, String callbackUrl);
+}
---
+++ lib/core/supabase/supabase_auth_client_impl.dart
@@ -0,0 +1,40 @@
+import 'package:craft_chain/core/supabase/auth_client.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseAuthClientImpl implements AuthClient{
+  @override
+  Future<void> resetPasswordForEmail({required String email}) {
+    // TODO: implement resetPasswordForEmail
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthResponse> signIn({required String email, required String password}) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> signInWithOAuth(OAuthProvider provider, String callbackUrl) {
+    // TODO: implement signInWithOAuth
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthResponse> signUp({required String email, required String password, required String name}) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserResponse> updatePassword({required String password}) {
+    // TODO: implement updatePassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthResponse> verifyPasswordResetOtp({required String email, required String otp}) {
+    // TODO: implement verifyPasswordResetOtp
+    throw UnimplementedError();
+  }
+}
