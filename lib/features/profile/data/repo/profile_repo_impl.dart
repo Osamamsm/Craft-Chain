1f5372d93dbb2969e5b9ce56849ff782aa80eebc
Osama Selim <osamaselim216@gmail.com>
2026-09-22 22:20:56 +0300

    feat(profile): add getUserProfile method to ProfileRepo interface and implementation

--- lib/features/profile/data/repo/profile_repo_impl.dart
+++ lib/features/profile/data/repo/profile_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:craft_chain/core/error/failures.dart';
 import 'package:craft_chain/features/profile/data/data_source/profile_remote_data_source.dart';
 import 'package:craft_chain/features/profile/data/models/complete_profile_params_model.dart';
 import 'package:craft_chain/features/profile/domain/entities/complete_profile_params.dart';
+import 'package:craft_chain/features/profile/domain/entities/user_profile_entity.dart';
 import 'package:craft_chain/features/profile/domain/repo/profile_repo.dart';
 import 'package:dartz/dartz.dart';
 
@@ -23,5 +24,11 @@ class ProfileRepoImpl implements ProfileRepo {
     } catch (e) {
       return Left(ExceptionMapper.mapExceptionToFailure(e));
     }
+  }
+
+  @override
+  Future<Either<Failure, UserProfileEntity>> getUserProfile({String? userId}) {
+    // TODO: implement getUserProfile
+    throw UnimplementedError();
   }
 }
--- lib/features/profile/domain/repo/profile_repo.dart
+++ lib/features/profile/domain/repo/profile_repo.dart
@@ -1,9 +1,12 @@
 import 'package:craft_chain/core/error/failures.dart';
 import 'package:craft_chain/features/profile/domain/entities/complete_profile_params.dart';
+import 'package:craft_chain/features/profile/domain/entities/user_profile_entity.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class ProfileRepo {
   Future<Either<Failure, void>> completeProfile({
     required CompleteProfileParams params,
   });
+
+  Future<Either<Failure, UserProfileEntity>> getUserProfile({String? userId});
 }
